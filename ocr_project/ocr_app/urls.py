7aef7d2465ea34a79e04860ee548e4603d6e0c24
Antonio Takev <tonitakev@gmail.com>
2019-04-04 23:25:23 +0300

    user registration changes 1 -> add a view for registration form 2 -> specify the url for registration 3-> make the redirection in case the user successfully register

--- ocr_project/ocr_app/urls.py
+++ ocr_project/ocr_app/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
 from . import views
-
+from ocr_app.views import UserRegistrationView
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^about/$', views.about, name='about'),    
+    url(r'^about/$', views.about, name='about'),
+    url(r'^register/$', view=UserRegistrationView.as_view(), name='register'),
+    url(r'^success_register/$', views.success_register, name='success_register'),
 ]
--- ocr_project/ocr_app/views.py
+++ ocr_project/ocr_app/views.py
@@ -4,7 +4,34 @@
 from __future__ import unicode_literals
 from django.shortcuts import render
 
+from django.views.generic import CreateView
+from ocr_app.forms import UserRegistrationForm
 # ---Main page views---
+
+#----- User registration views ----
+class UserRegistrationView(CreateView):
+    is_registered = False
+    form_class = UserRegistrationForm
+    template_name = "register.html"
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated():
+            return HttpResponseForbidden()
+        return super(UserRegistrationView, self).dispatch(request, *args, **kwargs)
+
+
+    def form_valid(self, form):
+        user = form.save(commit=False)
+        user.set_password(form.cleaned_data['password'])
+        user.save()
+        success_url=('success_register')
+        return redirect(success_url)
+
+
+def success_register(request):
+    return render(request, 'success_register.html')
+
+
 
 #home page
 def index(request):
