2c088b5ce44e1fa79accd5a179cc380feb3adccd
Antonio Takev <tonitakev@gmail.com>
2019-04-07 21:16:04 +0300

    Dir changes make a static folder and with a css folder in it change the url for media and image folders delete the repeated folders check the structure

--- ocr_project/ocr_project/settings.py
+++ ocr_project/ocr_project/settings.py
@@ -117,11 +117,16 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    '/ocr_project/ocr_app/static/',
+]
+
 LOGIN_URL = reverse_lazy('login')
 LOGIN_REDIRECT_URL = reverse_lazy('profile')
 LOGOUT_REDIRECT_URL = reverse_lazy('login')
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join('ocr_app', 'media')
